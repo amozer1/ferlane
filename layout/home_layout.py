baf690329ebcee28846d3d11b998af03002b4abc
Ebenezer Amoako <amozer2003@yahoo.co.uk>
2026-05-17 15:33:27 +0100

    Updated dashboard

--- layout/home_layout.py
+++ layout/home_layout.py
@@ -7,33 +7,18 @@ from cards.table_card import render_table
 
 def render_home(result, df32):
 
-    col1, col2 = st.columns([1, 1])
+    col1, col2 = st.columns(2)
 
     with col1:
-        st.markdown("""
-        <div class="dashboard-card">
-            <div class="card-title">📊 Schedule Summary (CL32)</div>
-        """, unsafe_allow_html=True)
-
+        st.container(border=True)
+        st.subheader("📊 Schedule Summary (CL32)")
         render_pie(df32)
 
-        st.markdown("</div>", unsafe_allow_html=True)
-
     with col2:
-        st.markdown("""
-        <div class="dashboard-card">
-            <div class="card-title">🔴 Delayed Activities (CL32)</div>
-        """, unsafe_allow_html=True)
-
+        st.container(border=True)
+        st.subheader("🔴 Delayed Activities (CL32)")
         render_delayed_table(df32)
 
-        st.markdown("</div>", unsafe_allow_html=True)
-
-    st.markdown("""
-    <div class="dashboard-card">
-        <div class="card-title">📋 Deliverable Register (CL31 vs CL32)</div>
-    """, unsafe_allow_html=True)
-
+    st.container(border=True)
+    st.subheader("📋 Deliverable Register (CL31 vs CL32)")
     render_table(result)
-
-    st.markdown("</div>", unsafe_allow_html=True)
